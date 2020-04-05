95dbc424ee71649aa2a05caf17cbbabf8112c7db
Ashish Kumar <ag.ashishaggarwal@gmail.com>
2020-04-05 19:19:25 +0530

    Start clothing project

---
+++ src/components/directory-menu/directory-menu.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './directory-menu.styles.scss';
+
+const DirectoryMenu = () => {
+    return (
+        <div>
+            Directory Menu
+        </div>
+    )
+} 
+
+export default DirectoryMenu;
---
+++ src/components/header/header.compoment.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ReactComponent as Logo } from './../../assets/crown.svg';
+import './header.styles.scss';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <Logo />
+            </div>
+            <div className="options">
+                <div className="option">
+                    Shop    
+                </div>
+
+                <div className="option">
+                    Contact    
+                </div>
+
+                <div className="option">
+                    SignIn    
+                </div>
+
+                <div className="option">
+                    Cart
+                </div>
+            </div>
+        </div>   
+    )
+}
+
+export default Header;
