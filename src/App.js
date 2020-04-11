a19b585bace7e3c366fdd53a21398db1430288e7
Ashish Kumar <ag.ashishaggarwal@gmail.com>
2020-04-11 22:41:49 +0530

    Shop page, collectio-preview and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import {Switch,Route, Link} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 import Homepage from './pages/homepage/homepage.component.jsx';
-
-const HatsPage = (props) => (
-  <div>
-    Hats Page
-    {/* <button onClick={() => props.history.push(`${props.match.url}/2`)}>Detail Page - 2</button>
-    <Link to={`${props.match.url}/3`}>Detail Page - 3</Link>
-    <Link to={`${props.match.url}/4`}>Detail Page - 4</Link> */}
-  </div>
-)
-
-const HatsDetailPage = (props) => (
-  <div>
-    Hats Detail Page - {props.match.params.hatId}
-  </div>
-)
 
 class App extends React.Component {
   
@@ -26,8 +12,7 @@ class App extends React.Component {
       <div>
         <Switch>
           <Route exact path="/" component={Homepage} />  
-          <Route exact path="/shop/:catrgory" component={HatsPage} />
-          <Route path="/shop/hats/:hatId" component={HatsDetailPage} />
+          <Route exact path="/shop" component={ShopPage} />
         </Switch>
       </div>
     )
